21cff4d0bc8a224391f9641a81dd98854efbd188
hxgst2 <hxgst2@yahoo.com>
2017-08-30 10:26:15 -0400

    update

---
+++ Test1_ConstVsReadOnly/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1_ConstVsReadOnly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var val = new TestClassReadOnly().ReadOnlyVariable;
+
+        }
+    }
+
+    public class TestClassReadOnly
+    {
+        public  readonly int ReadOnlyVariable=2;
+        public TestClassReadOnly()
+        {
+            // Value assignment is allowed in Ctr only
+            ReadOnlyVariable = 4;
+        }
+
+        public void TestReadOnlyVariableMethod()
+        {
+            // Value assignment not allowed in Methods
+            // ReadOnlyVariable = 4;
+        }
+    }
+}
